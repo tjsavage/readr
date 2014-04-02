3ec3d2d8947e47999f0bf93c4740d261cbe62a34
Esthena <esthenabarlow@gmail.com>
2014-04-01 22:23:03 -0700

    failing essay test

--- models/essay.js
+++ models/essay.js
@@ -4,20 +4,35 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var essaySchema = new Schema({
-	id: ObjectId,
+	id: Schema.ObjectId,
 	status: String,
 	school: String,
 	prompt: String,
-	submitter_id: ObjectId,
+	submitter_id: Schema.ObjectId,
 	review_ids: Array,
 	date_submitted: Date,
 	date_closed: Date,
 	date_due: Date,
 	feedback_type: String,
 	surge: Boolean,
-	document_id: ObjectId
+	document_id: Schema.ObjectId
 });
 
 var Essay = mongoose.model('Essay', essaySchema);
 
-module.exports = Essay;
+
+
+essaySchema.methods.setStatus = function(status) {
+	var statusOptions = ['submitted','claimed','reviewed','returned','closed']
+	if (statusOptions.contains(status)){
+		return;
+	}
+	else {
+		throw "invalid status";
+		return;
+	}
+
+}
+
+mongoose.model('Essay', essaySchema)
+module.exports = mongoose.model('Essay', essaySchema);
