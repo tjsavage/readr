bd56c5ac3baf5f90576010a12f3fd473a1702767
Taylor Savage <tjsavage@google.com>
2014-04-15 10:17:41 -0700

    added formidable for multipart form parsing

--- routes/essays.js
+++ routes/essays.js
@@ -1,10 +1,20 @@
 // routes/essays.js
+var formidable = require('formidable');
+var util = require('util');
 
 exports.get_submit = function(req, res) {
 	res.render("essays/submit.html")
 };
 
 exports.post_submit = function(req, res) {
-    console.log(req.body);
-    res.send(200);
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('received upload:\n\n');
+        res.end(util.inspect({fields: fields, files: files}));
+    });
+
+    return;
+
 };
